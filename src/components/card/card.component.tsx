cf314079a3835a2366f416d10001369cde111cd7
OsimFavour <famos204@gmail.com>
2024-11-30 22:28:47 +0100

    Migrate card component jsx to tsx

---
+++ src/components/card/card.component.tsx
@@ -0,0 +1,49 @@
+import { Monster } from '../../App'
+
+import './card.styles.css'
+
+type CardProps = {
+  monster: Monster
+
+}
+
+const Card = ({ monster }: CardProps) => {
+  const { id, name, email } = monster
+  return (
+  
+    <div className='card-container' key={id}>
+      <img 
+          alt={`monster ${name}`} 
+          src={`https://robohash.org/${id}?set=set2&size=180x180`}
+      />
+
+      <h2>{name}</h2>
+      <p>{email}</p>
+    </div>
+    
+  )
+}
+
+
+// class Card extends Component {
+//   render() {
+
+//     const { name, email, id } = this.props.monster
+
+//     return (
+    
+//     <div className='card-container' key={id}>
+//         <img 
+//             alt={`monster ${name}`} 
+//             src={`https://robohash.org/${id}?set=set2&size=180x180`}
+//         />
+
+//         <h2>{name}</h2>
+//         <p>{email}</p>
+//     </div>
+    
+//     )
+//   }
+// }
+
+export default Card
